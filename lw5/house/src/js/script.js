0905e20e928bd2651e54a21aac74c29a6103317c
Rustam <rustamikc@gmail.com>
2022-06-07 16:08:37 +0300

    lw5 house доделал

--- lw5/house/src/js/script.js
+++ lw5/house/src/js/script.js
@@ -18,7 +18,10 @@ function draw() {
 
 function drawHouse() {
 	drawWalls();
-	drawRoofBlock();
+	drawWindows();
+	drawRoof();
+	drawDoor(-202, 70, 50, 0);
+	drawGarageDoor(-202, -30, 50, 0);
 }
 
 function init() {
@@ -26,7 +29,7 @@ function init() {
 	renderer.setClearColor(0xcce0ff, 1);
 	document.body.appendChild(renderer.domElement);
 
-	camera.position.set(500, 200, 200);
+	camera.position.set(500, 500, 200);
 	camera.lookAt(scene.position);
 
 	const light = new THREE.DirectionalLight(0xffffff, 1);
@@ -75,6 +78,15 @@ function drawWalls() {
 	drawWallsBlock(0, 100, 200, 0, 1);
 }
 
+function drawWindows() {
+	drawWindow(2, 100, 180, 0);
+	drawWindow(2, -30, 80, 0);
+	drawWindow(2, 100, 80, 0);
+
+	drawWindow(-202, 100, 180, 0);
+	drawWindow(-202, 150, 80, 0);
+}
+
 function drawWallsBlock(z, x, y, r, type = 0) {
 	const shape = new THREE.Shape();
 	if(type === WALL_TYPES.SQUARE) {
@@ -107,16 +119,72 @@ function drawWallsBlock(z, x, y, r, type = 0) {
 	house.add(wall);
 }
 
-function drawRoofBlock() {
-	const geometry = new THREE.BoxGeometry( 300, 200, 0 );
+function drawWindow(z, x, y, r) {
+	const geometry = new THREE.BoxGeometry(63, 49, 0);
+
+	const texture = new THREE.TextureLoader().load('./src/img/window.png');
+	texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+
+	var material = new THREE.MeshBasicMaterial({map: texture});
+
+	const wall = new THREE.Mesh(geometry, material);
+	wall.position.z = z;
+	wall.position.x = x;
+	wall.position.y = y;
+	wall.rotation.y = Math.PI * r;
+
+	house.add(wall);
+}
+
+function drawDoor(z, x, y, r) {
+	const geometry = new THREE.BoxGeometry(45, 90, 0);
+
+	const texture = new THREE.TextureLoader().load('./src/img/door.png');
+	texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+
+	var material = new THREE.MeshBasicMaterial({map: texture});
+
+	const wall = new THREE.Mesh(geometry, material);
+	wall.position.z = z;
+	wall.position.x = x;
+	wall.position.y = y;
+	wall.rotation.y = Math.PI * r;
+
+	house.add(wall);
+}
+
+function drawGarageDoor(z, x, y, r) {
+	const geometry = new THREE.BoxGeometry(110, 100, 0);
+
+	const texture = new THREE.TextureLoader().load('./src/img/garage-door.png');
+	texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+
+	var material = new THREE.MeshBasicMaterial({map: texture});
+
+	const wall = new THREE.Mesh(geometry, material);
+	wall.position.z = z;
+	wall.position.x = x;
+	wall.position.y = y;
+	wall.rotation.y = Math.PI * r;
+
+	house.add(wall);
+}
+
+function drawRoof() {
+	drawRoofGarageSide();
+	drawRoofShortSide();
+}
+
+function drawRoofGarageSide() {
+	const geometry = new THREE.BoxGeometry(283, 200, 0);
 
 	const texture = new THREE.TextureLoader().load('./src/img/roof.jpg');
 	texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-	texture.repeat.set( 3, 2  );
+	texture.repeat.set(3, 2);
 	texture.rotation = Math.PI / 2;
-	const textureMaterial = new THREE.MeshPhongMaterial({ map: texture});
+	const textureMaterial = new THREE.MeshPhongMaterial({map: texture});
 
-	const roof = new THREE.Mesh( geometry, textureMaterial );
+	const roof = new THREE.Mesh(geometry, textureMaterial);
 	house.add(roof);
 
 	roof.rotation.x = Math.PI / 2;
@@ -127,6 +195,28 @@ function drawRoofBlock() {
 
 	return roof;
 }
+
+function drawRoofShortSide() {
+	const geometry = new THREE.BoxGeometry(141, 200, 0);
+
+	const texture = new THREE.TextureLoader().load('./src/img/roof.jpg');
+	texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+	texture.repeat.set(2, 2);
+	texture.rotation = Math.PI / 2;
+	const textureMaterial = new THREE.MeshPhongMaterial({map: texture});
+
+	const roof = new THREE.Mesh(geometry, textureMaterial);
+	house.add(roof);
+
+	roof.rotation.x = Math.PI / 2;
+	roof.rotation.y = -Math.PI / 4;
+	roof.position.y = 250;
+	roof.position.x = 150;
+	roof.position.z = -100;
+
+	return roof;
+}
+
 function drawGrass() {
 	const geometry = new THREE.PlaneGeometry(10000, 10000);
 
